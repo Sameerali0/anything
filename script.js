21e5b9bf1c9a0af86fe4064d56540b921f609c40
Sameerali0 <sameer.ali.professional@gmail.com>
2025-12-06 15:02:04 +0500

    added slider function to js and i working on project card sliding

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const projectCards= document.querySelectorAll(".project-card")
 const dotsContainer= document.querySelector("slider-dots")
+const sliderRow= document.querySelector(".slider-row")
 
+let currentIndex= 0
 const totalCards= projectCards.length
 
 for(let i= 0; i < totalCards; i++){
@@ -17,7 +19,40 @@ for(let i= 0; i < totalCards; i++){
 
     })
 
-    dotsContainer.appendChild(dot);
+    dotsContainer.appendChild(dot)
+
 }
 
-const dots = document.querySelectorAll(".dot");
+const dots= document.querySelectorAll(".dot")
+
+function slider(){
+
+    projectCards.forEach((card, index) =>{
+
+        card.classList.remove("prev", "active", "next")
+
+        if(index === currentIndex){
+
+            card.classList.add("active")
+
+        }else if (index === currentIndex -1 || (currentIndex === 0 && index === totalCards - 1)){
+
+            card.classList.add("prev")
+
+        }else if (index === currentIndex +1 || (currentIndex === totalCards - 1 && index === 0)){
+
+            card.classList.add("next")
+
+        }
+    })
+
+    const cardWidth= projectCards[0].offsetWidth + 20
+    const sliderShift= -currentIndex * cardWidth + cardWidth
+    sliderRow.style.transform= `translateX(${sliderShift}px)`
+
+    dots.forEach(dot => dot.classList.remove("active"))
+    dots[currentIndex].classList.add("active")
+
+}
+
+slider()
