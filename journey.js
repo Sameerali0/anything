010246f26d823185990cad89c972bbb44d779a8e
Sameerali0 <sameer.ali.professional@gmail.com>
2025-12-13 00:57:28 +0500

    make bar move when play, stop on pause, go back on rewind

--- journey.js
+++ journey.js
@@ -1,4 +1,55 @@
 const playBtn= document.getElementById("playBtn")
 const rewindBtn= document.getElementById("rewindBtn")
 const pauseBtn= document.getElementById("pauseBtn")
+const bar= document.getElementById("bar")
+
+let barPrgress= 0
+let timer
+
+playBtn.addEventListener("click", ()=>{
+
+    clearInterval(timer)
+
+    timer= setInterval(()=>{
+
+        if(barPrgress >= 100){
+
+            clearInterval(timer)
+        }else{
+
+            barPrgress += 1
+            bar.style.width= barPrgress + "%"
+
+        }
+
+    }, 50)
+
+})
+
+
+pauseBtn.addEventListener("click", ()=>{
+
+    clearInterval(timer)
+
+})
+
+
+rewindBtn.addEventListener("click", ()=>{
+    clearInterval(timer)
+
+    timer= setInterval(()=>{
+
+        if(barPrgress <= 0){
+            clearInterval(timer)
+
+        }else{
+
+            barPrgress -= 1
+            bar.style.width= barPrgress +"%"
+
+        }
+
+    }, 50)
+
+})
 
